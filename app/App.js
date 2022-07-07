1804d21facae701338fbe3ea646af6368cf6b992
Waldemar (velrok) <34974+Velrok@users.noreply.github.com>
2022-07-07 20:09:19 +0100

    fetch and display a buch of tickets and their summaries

--- app/App.js
+++ app/App.js
@@ -1,35 +1,53 @@
 const React = require("react");
 const { useState, useEffect } = require("react");
-const { render, Text, Box, useApp, useInput } = require("ink");
+const { render, Text, Box, useApp, useInput, useFocusManager, useFocus } = require("ink");
 
-const { getTickets } = require("./jira");
+const { fetch_issues } = require("./jira");
 
 const AppTitle = () => <Text>Jira</Text>;
 
+const JiraIssue = ({ issue }) => {
+  const { isFocused } = useFocus();
+  return <Box>
+    <Text bold={isFocused}>
+      <Text inverse>{issue.key}</Text>
+      <Text> {issue.fields.summary}</Text>
+    </Text>
+  </Box>
+}
+// <Text>{issue.fields.assignee.name}</Text>
+// <Text>{issue.fields.status}</Text>
+// <Text>{issue.fields.creator}</Text>
+// <Text>{issue.fields.resporter}</Text>
+// <Text>{issue.fields.issuetype}</Text>
+// <Text>{issue.fields.project}</Text>
+
 const App = () => {
   const { exit } = useApp();
-  const [tickets, setTickets] = useState([]);
+  const [issues, setIssues] = useState([]);
 
   useEffect(() => {
-    getTickets().then((tickets) => {
-      console.log(tickets);
-      setTickets(tickets);
+    fetch_issues().then((data) => {
+      const issues = data.issues
+      setIssues(issues);
     });
   }, []);
 
+  const {focusPrevious, focusNext} = useFocusManager()
+
   useInput((input, _key) => {
     input === "q" && exit();
-    // input === "j" && focusNext();
-    // input === "k" && focusPrevious();
+    input === "j" && focusNext();
+    input === "k" && focusPrevious();
     // input === "/" && focus(SEARCH_FOCUS_ID);
   });
 
   return (
-    <Box flexDirection="row" height={"100%"} flexGrow={1} borderStyle="single">
+    <Box flexDirection="column" height={"100%"} flexGrow={1} borderStyle="single">
       <AppTitle></AppTitle>
-      <Box>
-        {tickets.map((ticket, idx) => (
-          <Text key={`ticket-list-${idx}`}>{ticket}</Text>
+      <Box flexDirection="column" borderStyle="single">
+        {issues.map((issue, idx) => (
+          <JiraIssue key={issue.key} issue={issue} />
         ))}
       </Box>
     </Box>
--- app/jira.js
+++ app/jira.js
@@ -1,22 +1,38 @@
-const { Version3Client } = require("jira.js");
+// {
+//   host: process.env.JIRA_HOST,
+//   authentication: {
+//     personalAccessToken: process.env.PERSONAL_ACCESS_TOKEN,
+//   },
+// }
 
-const client = new Version3Client({
-  host: process.env.JIRA_HOST,
-  authentication: {
-    personalAccessToken: process.env.PERSONAL_ACCESS_TOKEN,
-  },
-});
+const base64 = require('base-64');
+const fetch = require('node-fetch');
 
-const getTickets = async () => {
-  // NOTE: this client is super annoying to understand
-  response = client.issueSearch({
-    jql: "project = MER",
-  });
-  // response = await client.issueSearch.searchForIssuesUsingJql({
-  //   jql: "project = MER",
-  // });
-  console.log(response);
-  return response.sections[0].issues;
-};
+const credentials = process.env.JIRA_USER_NAME + ":" + process.env.JIRA_PERSONAL_ACCESS_TOKEN
 
-module.exports = { getTickets };
+const request = (path, method= 'GET', extra_options= {}) => {
+  const options = {
+    method: method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + base64.encode(credentials)
+    }
+  }
+  
+  // const url = process.env.JIRA_HOST + '/rest/api/3/issue/MER-1142'
+  const url = process.env.JIRA_HOST + path //'/rest/api/3/myself'
+  return fetch(url, {...options, ...extra_options})
+    .then(response => response.json())
+}
+
+const fetch_issues = (page=0) => {
+  const page_size = 50
+  return request('/rest/api/3/search?jql=project%20%3D%20'
+    + process.env.JIRA_PROJECT
+    + '&maxResults=' + page_size
+    + '&startAt=' + page_size * page
+  )
+}
+
+module.exports = { fetch_issues };
